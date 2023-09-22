1c53478cb162d847cbdeabadabdec2af88560458
Akiyoshi Suda <aki.suda@protonmail.com>
2023-09-22 21:37:35 +0900

    Fix rev

--- packages/torch-geometric.nix
+++ packages/torch-geometric.nix
@@ -12,18 +12,15 @@
 , psutil
 }:
 
-let
-  version = "2.3.1";
-in
 buildPythonPackage {
-  inherit version;
   pname = "torch-geometric";
+  version = "2.3.1";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "pyg-team";
     repo = "pytorch_geometric";
-    rev = "v${version}";
+    rev = "ff9fb3d7cdbaa2f2b8a848fb6cc5f4c3f465118c";
     sha256 = "sha256-69I54tVnPLz0GSpvgFxhiUkvrUqutRxfBi1TULjfqgw=";
   };
 
